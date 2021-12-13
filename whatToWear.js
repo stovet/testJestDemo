aa82789d11531f0ed4a551a4d4f75b682006202b
stovet <steven.topalian@gmail.com>
2021-12-13 13:44:29 -0500

    finished reformatting toEqual and return statments

--- whatToWear.js
+++ whatToWear.js
@@ -14,20 +14,26 @@ function whatToWear(eventType, temperature){
     }
     if(temperature > 53 && temperature < 70){
         if(eventType === "casual"){
-            return `Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and a jacket`;
+            console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and a jacket`);
+            return `${eventType} ${temperature}`;
         } else if (eventType === "semi-formal"){
-            return `Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and a jacket`;
+            console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and a jacket`);
+            return `${eventType} ${temperature}`;
         } else if (eventType === "formal"){
-            return `Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and a jacket`;
+            console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and a jacket`);
+            return `${eventType} ${temperature}`;
         }
     }
     if(temperature > 70){
         if(eventType === "casual"){
-            return `Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and no jacket`;
+            console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and no jacket`);
+            return `${eventType} ${temperature}`;
         } else if (eventType === "semi-formal"){
-            return `Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and no jacket`;
+            console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and no jacket`);
+            return `${eventType} ${temperature}`;
         } else if (eventType === "formal"){
-            return `Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and no jacket`;
+            console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and no jacket`);
+            return `${eventType} ${temperature}`;
         }
     }
 }
--- whatToWear.test.js
+++ whatToWear.test.js
@@ -21,21 +21,21 @@ describe("What to wear", function(){
         expect(whatToWear("formal", 52)).toEqual("formal " + 52);
     });
     test("when to wear something comfy and a jacket", function(){
-        expect(whatToWear("casual", 55)).toEqual(`Since it is 55 degrees and you are going to a casual event, you should wear something comfy and a jacket`);
+        expect(whatToWear("casual", 55)).toEqual("casual " + 55);
     })
     test("when to wear a polo and a jacket", function(){
-        expect(whatToWear("semi-formal", 68)).toEqual(`Since it is 68 degrees and you are going to a semi-formal event, you should wear a polo and a jacket`);
+        expect(whatToWear("semi-formal", 68)).toEqual("semi-formal " + 68);
     })
     test("when to wear only'a suit' and a jacket", function(){
-        expect(whatToWear("formal", 60)).toEqual("Since it is 60 degrees and you are going to a formal event, you should wear a suit and a jacket");
+        expect(whatToWear("formal", 60)).toEqual("formal " + 60);
     });
     test("when to wear something comfy and 'no jacket'", function(){
-        expect(whatToWear("casual", 71)).toEqual(`Since it is 71 degrees and you are going to a casual event, you should wear something comfy and no jacket`);
+        expect(whatToWear("casual", 71)).toEqual("casual " + 71);
     })
     test("when to wear a polo and 'no jacket'", function(){
-        expect(whatToWear("semi-formal", 79)).toEqual(`Since it is 79 degrees and you are going to a semi-formal event, you should wear a polo and no jacket`);
+        expect(whatToWear("semi-formal", 79)).toEqual("semi-formal " + 79);
     })
     test("when to wear 'a suit' and 'no jacket'", function(){
-        expect(whatToWear("formal", 100)).toEqual(`Since it is 100 degrees and you are going to a formal event, you should wear a suit and no jacket`);
+        expect(whatToWear("formal", 100)).toEqual("formal " + 100);
     });
 })
