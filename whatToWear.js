ff88c16195b0eee31a13afc0b38e20032880e317
stovet <steven.topalian@gmail.com>
2021-12-13 14:12:58 -0500

    cleaned up return

--- whatToWear.js
+++ whatToWear.js
@@ -3,37 +3,37 @@ function whatToWear(eventType, temperature){
     if(temperature < 54){
         if(eventType === "casual"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and a coat`);
-            return "something comfy " + "a coat";
+            return "something comfy a coat";
         } else if (eventType === "semi-formal"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and a coat`);
-            return "polo " +  "a coat";
+            return "polo a coat";
         } else if (eventType === "formal"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and a coat`);
-            return "suit " + "a coat";
+            return "suit a coat";
         }
     }
     if(temperature > 53 && temperature < 70){
         if(eventType === "casual"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and a jacket`);
-            return "something comfy " + "a jacket";
+            return "something comfy a jacket";
         } else if (eventType === "semi-formal"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and a jacket`);
-            return "polo " +  "a jacket";
+            return "polo a jacket";
         } else if (eventType === "formal"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and a jacket`);
-            return "suit " + "a jacket";
+            return "suit a jacket";
         }
     }
     if(temperature > 70){
         if(eventType === "casual"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear something comfy and no jacket`);
-            return "something comfy " + "no jacket";
+            return "something comfy no jacket";
         } else if (eventType === "semi-formal"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a polo and no jacket`);
-            return "polo " +  "no jacket";
+            return "polo no jacket";
         } else if (eventType === "formal"){
             console.log(`Since it is ${temperature} degrees and you are going to a ${eventType} event, you should wear a suit and no jacket`);
-            return "suit " + "no jacket";
+            return "suit no jacket";
         }
     }
 }
